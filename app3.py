2f6e7185d877b0cad64e8af9134301328f713bb3
mohammad.junaidm <mohammad.junaidm@telusinternational.com>
2025-05-29 15:26:07 +0530

    Update app3.py

--- app3.py
+++ app3.py
@@ -1036,5 +1036,5 @@ def health_check():
 
 if __name__ == "__main__":
     logger.info("Starting Flask application...")
-    app.run(debug=True, host="0.0.0.0", port=5003)
+    app.run(debug=True, host="0.0.0.0", port=5000)
 
